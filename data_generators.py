523645afbf8e373664ee85090d72bc39cf669be4
Mortezaei <58666136+zahraMRTZ@users.noreply.github.com>
2024-07-01 01:27:47 +0330

    Update data_generators.py

--- data_generators.py
+++ data_generators.py
@@ -13,7 +13,8 @@ import cv2
 from skimage.measure import regionprops
 from shutil import copyfile
 import tensorflow as tf
-
+import pydicom as dicom
+import sys
 
 # Custom Model Data Generator for Non-Serial Data
 #data_xlsx = '/content/drive/MyDrive/MasterThesis/ProstateMR_USSL/Datasets/metadata.xlsx'
@@ -27,7 +28,7 @@ def custom_data_generator(data_xlsx, train_obj='zonal', probabilistic=False, tes
     2)  Compatible with tf.data.Dataset Wrappers for Python Generators.
     """
     # Load I/O Datasheet + Initialize Arrays
-    all_data   = pd.read_excel(data_xlsx)
+    all_data   = pd.read_excel(data_xlsx) #data_xlsx = TRAIN_XLSX
     
     i = 0
     while True:
@@ -40,11 +41,11 @@ def custom_data_generator(data_xlsx, train_obj='zonal', probabilistic=False, tes
                 
                 # Anatomical Segmentation (WG,TZ,PZ)
                 if (train_obj=='zonal'):
-                    image  = np.load(all_data['image_path'][i])
+                    image  = dicom.dcmread(all_data['image_path'][i])
                     if image.ndim == 4: image = image[:,:,:,:1]
                     # image  = image[..., np.newaxis]   
                     if not test:
-                        zones  = np.load(all_data['zones_path'][i]).astype(np.uint8)
+                        zones  = dicom.dcmread(all_data['zones_path'][i]).astype(np.uint8)
                         tz, pz = zones.copy() , zones.copy()  
                         tz[zones!=1], pz[zones!=2] = 0,0 
                         tz[zones==1], pz[zones==2] = 1,1                                       # Binarize TZ/PZ Annotations Independently
@@ -54,10 +55,11 @@ def custom_data_generator(data_xlsx, train_obj='zonal', probabilistic=False, tes
                         label  = np.stack([np.zeros_like(image[...,0]), np.zeros_like(image[...,0]), np.zeros_like(image[...,0])], axis=-1)
                 # Diagnostic Segmentation (csPCa)
                 if (train_obj=='lesion'):
-                    image   = np.load(all_data['image_path'][i])    
-                    lesions = np.load(all_data['label_path'][i])
-                    lesions[lesions<=1] = 0                         
-                    lesions[lesions>=2] = 1                                                # Binarize Annotation (csPCa: GGG>=2)
+                    image   = dicom.dcmread(all_data['image_path'][i]).pixel_array   
+                    lesions = dicom.dcmread(all_data['label_path'][i]).pixel_array
+                    #print(lesions.dtype)
+                    #lesions[lesions<=1] = 0                         
+                    #lesions[lesions>=2] = 1                                                # Binarize Annotation (csPCa: GGG>=2)
                     lesions = contour_smoothening(lesions)                                 # Smoothen Contour Definitions
                     label   = np.stack([np.ones_like(lesions)-lesions, lesions], axis=-1)  # One-Hot Encoding
                 break
@@ -74,8 +76,9 @@ def custom_data_generator(data_xlsx, train_obj='zonal', probabilistic=False, tes
         # Standard/Deterministic Segmentation
         else:
             yield {"image":     image.copy()},{
-                   "detection": label.copy()}
-
+                   "detection": label.copy()},{
+                   "Dimention": image.ndim}
+  #print(data_xlsx)
 
 # Smoothen Annotation Contours Post-Resampling
 def contour_smoothening(label, kernel_2d=(7,7), iterations=1):
